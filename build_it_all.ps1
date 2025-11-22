a440ce44327c02f75daac45340901a70f5bd01d5
jakekohlhepp <jakekohlhepp@gmail.com>
2025-11-22 00:58:57 -0500

    Update for changes pre 2026 sections.

--- build_it_all.ps1
+++ build_it_all.ps1
@@ -1,8 +1,28 @@
-Get-ChildItem -Filter *.tex -Recurse | ForEach-Object {
-    $texPath = $_.FullName
-    $name = [System.IO.Path]::GetFileNameWithoutExtension($texPath)
+# Make sure we are running in the folder where this script lives
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $scriptDir
 
+Write-Host "Script directory: $scriptDir"
+
+# Find all beamer slide tex files
+$beamerFiles = Get-ChildItem -Path . -Filter 'slides_*.tex' -File
+
+Write-Host "Found $($beamerFiles.Count) beamer .tex files:"
+$beamerFiles | ForEach-Object { Write-Host " - $($_.Name)" }
+
+if ($beamerFiles.Count -eq 0) {
+    Write-Host "No slides_*.tex files found. Exiting."
+    return
+}
+
+foreach ($file in $beamerFiles) {
+    $texPath = $file.FullName
+    $name    = [System.IO.Path]::GetFileNameWithoutExtension($texPath)
+
+    Write-Host ""
+    Write-Host "==============================================="
     Write-Host "Compiling $texPath -> $name.pdf (xelatex)"
+    Write-Host "==============================================="
 
     # Build with XeLaTeX, non-interactive
     latexmk -xelatex -f `
